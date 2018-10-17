122b8a03dd611eaba487eba26fe2f83b36002588
张洪波 <zhanghongbo@anlewo.com>
2018-10-17 17:53:17 +0800

    Init

---
+++ src/Facades/Packagetest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Shawn\Packagetest\Facades;
+
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2018/10/17
+ * Time: 下午5:04
+ */
+
+class Packagetest extends \Illuminate\Support\Facades\Facade
+{
+    public static function getFacadeAccessor()
+    {
+        return 'packagetest';
+    }
+}
---
+++ src/Packagetest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2018/10/17
+ * Time: 下午5:02
+ */
+
+namespace Shawn\Packagetest;
+
+use Illuminate\Session\SessionManager;
+use Illuminate\Config\Repository;
+
+
+class Packagetest
+{
+    /**
+     * @var SessionManager
+     */
+    protected $session;
+    /**
+     * @var Repository
+     */
+    protected $config;
+    /**
+     * Packagetest constructor.
+     * @param SessionManager $session
+     * @param Repository $config
+     */
+    public function __construct(SessionManager $session, Repository $config)
+    {
+        $this->session = $session;
+        $this->config = $config;
+    }
+    /**
+     * @param string $msg
+     * @return string
+     */
+    public function test_rtn($msg = ''){
+        $config_arr = $this->config->get('packagetest.options');
+        return $msg.' <strong>from your custom develop package!</strong>>';
+    }
+
+}
